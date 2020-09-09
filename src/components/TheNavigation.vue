f04c3d3460f06f31b290a70423b8a79a6eadee86
rafaelrrs <rafaelsilva90@gmail.com>
2020-09-08 23:26:23 -0300

    02 commit - about

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,72 @@
+<template>
+  <nav id="nav">
+    <p class="logo">The Vue School Travell App</p>
+    <ul class="nav-links">
+      <li class="links">
+        <router-link to="/">
+          Home
+        </router-link>
+      </li>
+      <li
+        v-for="destination in destinations"
+        :key="destination.name"
+        class="links"
+      >
+        <router-link
+          :to="{
+            name: 'DestinationDetails',
+            params: { slug: destination.slug },
+          }"
+        >
+          {{ destination.name }}
+        </router-link>
+      </li>
+      <li class="links"><router-link to="/about">About</router-link></li>
+      <li class="links"><router-link to="/user">Dashboard</router-link></li>
+    </ul>
+  </nav>
+</template>
+<script>
+import store from "@/store";
+export default {
+  data() {
+    return {
+      destinations: store.destinations,
+    };
+  },
+};
+</script>
+<style scoped>
+#nav {
+  display: flex;
+  align-items: center;
+  position: sticky;
+  top: 0;
+  background-color: whitesmoke;
+  border-bottom: 1px solid grey;
+  z-index: 1;
+}
+#nav a {
+  color: #2c3e50;
+  text-decoration: none;
+  font-weight: bold;
+}
+#nav a.vue-school-active-class {
+  color: #ab26ab;
+}
+.nav-links {
+  display: flex;
+}
+.links {
+  padding-right: 20px;
+  list-style: none;
+}
+.links:hover {
+  text-decoration: underline;
+}
+.logo {
+  font-size: 20px;
+  color: #ab26ab;
+  font-weight: bold;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>
+      <h2>Vue Router for Everyone</h2>
+      <p>For more details, please, acess de 
+        <a href="https://vueschool.io/courses/vue-router-for-everyone" target="_blank">course</a>!</p>
+    </div>
   </div>
 </template>
+
+<style scoped>
+body {
+  background: #FFFFFF url(https://vueschool.io/media/9ea899b785de8ee088c77a71366e9678/master-the-vuejs-router-by-building-a-travel-app.png) no-repeat right top fixed;
+}
+</style>
